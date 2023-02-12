5f55b6d98b224b35d0831ce8f6b57946ff232130
Jon Doe <jondoe@doe.com>
2023-02-12 18:36:52 +0800

    step3- add random animal to an arry n display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import {useState } from 'react'
+
+
+const animals = ['Lion', 'Tiger', 'Elephant', 'Giraffe', 'Zebra'];
+
+const randomAnimal = () => {
+  const randomIndex = Math.floor(Math.random() * animals.length);
+  return animals[randomIndex];
+};
+
+
 function App(){
 
+    const [animalName, setAnimalName] = useState([]);
+ 
     const handleClick = () => {
-        console.log('Button was clicked!');
+        setAnimalName([...animalName,randomAnimal()]);
     }
 
     return <div>
         <button onClick={handleClick}>Add an Animal</button>
+        <div>Random Animal: {animalName}</div>
     </div>
 }
 
